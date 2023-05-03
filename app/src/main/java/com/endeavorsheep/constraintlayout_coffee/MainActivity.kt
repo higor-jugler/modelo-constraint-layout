e3c402fa9f1abe01e60b23c796cf1f77cca06285
Higor Jugler <higorjugler@hotmail.com>
2023-05-03 13:10:10 -0300

    Simples view para praticar conceitos de constrait layout

---
+++ app/src/main/java/com/endeavorsheep/constraintlayout_coffee/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.endeavorsheep.constraintlayout_coffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
